f6c1054ba6b2a95182d08782cd8ca57e88f66646
elessert760 <navydeltachi@gmail.com>
2020-01-30 16:36:20 -0800

    updates get_commit_subject

--- R/get_commit_subject.R
+++ R/get_commit_subject.R
@@ -2,15 +2,20 @@
 #'
 #' @return
 #' @export
-#'
+#' @importFrom crayon bold
 #' @examples
 #' get_commit_subject()
 #'
 get_commit_subject <- function() {
-  commit_subject <- readline("\nWhat is the subject of this commit? \n
-                             This should be a one line description of what this commit is about \n ")
+
+  x <- paste0("\nWhat is the subject of this commit? \n",
+  crayon::bold("This should be a one line description of what this commit is about \n "))
+  cat(x)
+
+  commit_subject <- readline()
   if(is.na(commit_subject) || commit_subject == "" || commit_subject == "[[:space:]]+"){
     commit_subject <- "no commit subject provided"
   }
   commit_subject
 }
+
