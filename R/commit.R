7b93407a80db151cf166509788e16779453f679b
elessert760 <navydeltachi@gmail.com>
2020-02-25 10:59:28 -0800

    Type: Issue - Subject: this is just a test - Changes:

--- R/commit.R
+++ R/commit.R
@@ -1,11 +1,23 @@
 commit <- function() {
+  changes <- system("git diff --stat", intern = T)
   type = get_commit_type()
   subject = get_commit_subject()
 
-  print(paste("Type:", type,
-              "- Subject:", subject,
-              "- Time:", Sys.time()
-              )
-        )
+  message <- paste("Type:", type,
+                   "- Subject:", subject,
+                   "- Changes:", paste(changes, collapse = " "))
+  cat(paste(
+    "The commit message will be: \n\n",
+    message,
+    " \n\nDo you wish to commit?"
+  ))
+
+  confirm <- utils::select.list(c("Yes", "No"))
+
+  if (confirm == "Yes") {
+    git2r::commit(message = message)
+  } else{
+    print("OK, aborting this commit")
+  }
 
 }
